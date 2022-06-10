ee1bfd3fe1ff8036bcdcfc52ecd36482fd4eb9cd
mchara01 <mc921@ic.ac.uk>
2022-06-10 18:43:34 +0300

    args added to BNG script

--- scripts/blockNumberGenerator.py
+++ scripts/blockNumberGenerator.py
@@ -1,8 +1,20 @@
 import requests
 import random
+import argparse
 
-SAMPLE_FILE = "blockNumbers.txt"
-BLOCK_SAMPLE_SIZE = 5
+# Argument parsing
+args = argparse.ArgumentParser(
+    description="Random block number generator."
+)
+args.add_argument("--size", dest="size", help="Block sample size", default=5)
+args.add_argument("--output", dest="output",
+                  help="Output file that will contain the generated block numbers", default="blockNumbers2.txt")
+#args.add_argument("--chain", dest="chain", help="Name of EVM-based chain to crawl (eth, bsc)", default="eth",)
+
+args = args.parse_args()
+
+SAMPLE_FILE = args.output
+BLOCK_SAMPLE_SIZE = int(args.size)
 block_sample = list()
 
 response = requests.get("https://api.blockcypher.com/v1/eth/main")
@@ -12,9 +24,10 @@ block_range = range(1, latest_block_eth + 1)
 block_sample = sorted(random.sample(block_range, BLOCK_SAMPLE_SIZE))
 
 with open(SAMPLE_FILE, 'w') as f:
-        for block in block_sample:
-            f.write("%s\n" % str(block))
-        print('Saved blocks sample of {} in file {} on disk.'.format(len(block_sample), SAMPLE_FILE))
-        percentage = len(block_sample)/latest_block_eth
-        print("Total number of blocks in blockchain: {}".format(latest_block_eth))
-        print("Percentage: {:.5f}%".format(percentage))
+    for block in block_sample:
+        f.write("%s\n" % str(block))
+    print('Saved blocks sample of {} in file {} on disk.'.format(
+        len(block_sample), SAMPLE_FILE))
+    percentage = len(block_sample)/latest_block_eth
+    print("Total number of blocks in blockchain: {}".format(latest_block_eth))
+    print("Percentage: {:.5f}%".format(percentage))
--- scripts/etherscanCrawl.py
+++ scripts/etherscanCrawl.py
@@ -5,9 +5,13 @@ import logging
 
 from etherscan import Etherscan
 
+ADDRESS_PRINT_INTERVAL = 3
+
+
 class Crawler:
     def __init__(self, api_key, chain):
-        logging.basicConfig(level=logging.INFO, format='[%(asctime)s] %(message)s')
+        logging.basicConfig(level=logging.INFO,
+                            format='[%(asctime)s] %(message)s')
         self.api_key = api_key
         self.chain = chain
         try:
@@ -28,14 +32,17 @@ class Crawler:
         self.cur.close()
         self.conn.close()
 
+
 def get_args():
     # Argument parsing
     args = argparse.ArgumentParser(
         prog="Crawler",
         description="Crawl blockchain explorer (EtherScan.io or BscScan.io) to get extra details about contracts."
     )
-    args.add_argument("--apikey", dest="api_key", help="API Key of blockchain explorer")
-    args.add_argument("--chain", dest="chain", help="Name of EVM-based chain to crawl (eth, bsc)", default="eth",)
+    args.add_argument("--apikey", dest="api_key",
+                      help="API Key of blockchain explorer")
+    args.add_argument("--chain", dest="chain",
+                      help="Name of EVM-based chain to crawl (eth, bsc)", default="eth",)
     #args.add_argument("output", help="JSON file path to save the results")
     return args.parse_args()
 
@@ -47,11 +54,10 @@ def main():
     # Addresses to crawl
     addresses = list()
 
-
     crawler = Crawler(args.api_key, args.chain)
     crawler.cur.execute(f"SELECT address FROM {crawler.chain}")
     for address in crawler.cur:
-        addresses.append(address[0])   
+        addresses.append(address[0])
     nr_contracts = len(addresses)
 
     # #results = read_json(output)
@@ -61,13 +67,12 @@ def main():
     #print(f"Output file            {output}")
     print()
 
-    ADDRESS_PRINT_INTERVAL = 3
     eth = Etherscan(crawler.api_key)
     values = list()
     balances = eth.get_eth_balance_multiple(addresses)
     sql_stmt = f"UPDATE {crawler.chain} SET nr_transactions = %s, balance = %s, nr_token_transfers = %s WHERE address = %s"
-    
-    for index,address in enumerate(addresses):
+
+    for index, address in enumerate(addresses):
         # Note : Some API endpoint returns a maximum of 10000 records only.
         # This is fine for us, as we only care if atleast one record exists.
         nr_transactions = 0
@@ -76,29 +81,35 @@ def main():
 
         if index % ADDRESS_PRINT_INTERVAL == 0:
             percent = index / len(addresses) * 100
-            print("Processing address {} / {} ({:.2f}% complete)".format(index, len(addresses), percent))
+            print("Processing address {} / {} ({:.2f}% complete)".format(index,
+                  len(addresses), percent))
 
         try:
-            nr_transactions = len(eth.get_normal_txs_by_address(address=address, startblock=0, endblock=99999999, sort="asc"))
+            nr_transactions = len(eth.get_normal_txs_by_address(
+                address=address, startblock=0, endblock=99999999, sort="asc"))
         except Exception as e:
             logging.info(f"{address} | Normal txs -- {e}")
 
         try:
-            erc20_tokens = len(eth.get_erc20_token_transfer_events_by_contract_address_paginated(contract_address=address, page=0, offset=0, sort="asc"))
+            erc20_tokens = len(eth.get_erc20_token_transfer_events_by_contract_address_paginated(
+                contract_address=address, page=0, offset=0, sort="asc"))
         except Exception as e:
             logging.info(f"{address} | ERC20 -- {e}")
 
         try:
-            erc721_tokens = len(eth.get_erc721_token_transfer_events_by_contract_address_paginated(contract_address=address, page=0, offset=0, sort="asc"))
+            erc721_tokens = len(eth.get_erc721_token_transfer_events_by_contract_address_paginated(
+                contract_address=address, page=0, offset=0, sort="asc"))
         except Exception as e:
             logging.info(f"{address} | ERC721 -- {e}")
 
         nr_token_transfers = erc20_tokens + erc721_tokens
-        values.append((nr_transactions, balances[index]['balance'], nr_token_transfers, address))
+        values.append(
+            (nr_transactions, balances[index]['balance'], nr_token_transfers, address))
 
     crawler.cur.executemany(sql_stmt, values)
     crawler.conn.commit()
     logging.info(f"{crawler.cur.rowcount} record(s) affected.")
+
 
 if __name__ == "__main__":
     main()
