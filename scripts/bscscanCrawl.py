0aab4965523eceb0da9ae671bd5162af2d38b9af
mchara01 <mc921@ic.ac.uk>
2022-06-26 16:49:07 +0300

    bscscan crawler async changes

--- scripts/bscscanCrawl.py
+++ scripts/bscscanCrawl.py
@@ -1,37 +1,12 @@
 import argparse
 import asyncio
 import logging
-import sys
 import time
 
-import mysql.connector
+import aiomysql
 from bscscan import BscScan
 
-ADDRESS_PRINT_INTERVAL = 3
-
-
-class Crawler:
-    def __init__(self, api_key):
-        logging.basicConfig(level=logging.INFO,
-                            format='[%(asctime)s] %(message)s')
-        self.api_key = api_key
-        try:
-            # Connect to MariaDB Platform
-            self.conn = mysql.connector.connect(
-                user="root",
-                password="Fm)4dj",
-                host="127.0.0.1",
-                port=3333,
-                database="db_blockchain"
-            )
-            self.cur = self.conn.cursor()
-        except mysql.connector.connect.Error as e:
-            print(f"Error connecting to MariaDB: {e}")
-            sys.exit(1)
-
-    def __del__(self):
-        self.cur.close()
-        self.conn.close()
+ADDRESS_PRINT_INTERVAL = 5
 
 
 def get_args():
@@ -46,32 +21,58 @@ def get_args():
     return args.parse_args()
 
 
-async def main():
+async def select(loop, sql, pool):
+    async with pool.acquire() as conn:
+        async with conn.cursor() as cur:
+            await cur.execute(sql)
+            response = await cur.fetchall()
+            return response
+
+
+async def update(loop, sql, pool, values):
+    async with pool.acquire() as conn:
+        async with conn.cursor() as cur:
+            await cur.executemany(sql, values)
+            await conn.commit()
+            return cur.rowcount
+
+
+async def main(loop):
     args = get_args()
     # output = args.output
-
+    api_key = args.api_key
     # Addresses to crawl
     addresses = list()
 
     start = time.time()
 
-    crawler = Crawler(args.api_key)
-    crawler.cur.execute(f"SELECT address FROM bsc")
-    for address in crawler.cur:
+    # crawler = Crawler(args.api_key)
+    # crawler.cur.execute(f"SELECT address FROM bsc")
+
+    pool = await aiomysql.create_pool(
+        user="root",
+        password="Fm)4dj",
+        host="127.0.0.1",
+        port=3333,
+        db="db_blockchain",
+        loop=loop)
+    response = await select(loop=loop, sql="SELECT address FROM bsc", pool=pool)
+
+    for address in response:
         addresses.append(address[0])
     nr_contracts = len(addresses)
 
     # #results = read_json(output)
 
-    print(f"BscScan API Key       {crawler.api_key}")
+    print(f"BscScan API Key         {api_key}")
     print(f"No. of contracts        {nr_contracts}")
     # print(f"Output file            {output}")
     print()
 
     values = list()
-    sql_stmt = f"UPDATE bsc SET nr_transactions = %s, balance = %s, nr_token_transfers = %s WHERE address = %s"
+    # sql_stmt = f"UPDATE bsc SET nr_transactions = %s, balance = %s, nr_token_transfers = %s WHERE address = %s"
 
-    async with BscScan(crawler.api_key) as client:
+    async with BscScan(api_key) as client:
         balances = await client.get_bnb_balance_multiple(addresses=addresses)
         for index, address in enumerate(addresses):
             # Note : Some API endpoint returns a maximum of 10000 records only.
@@ -105,15 +106,25 @@ async def main():
             nr_token_transfers = bep20_tokens + bep721_tokens
             values.append((nr_transactions, balances[index]['balance'], nr_token_transfers, address))
 
-        crawler.cur.executemany(sql_stmt, values)
-        crawler.conn.commit()
+    # crawler.cur.executemany(sql_stmt, values)
+    # crawler.conn.commit()
 
-        end = time.time()
-        elapsed = end - start
-        logging.info("Finished crawling bscscan")
-        logging.info(f"{crawler.cur.rowcount} record(s) affected.")
-        logging.info(f"Elapsed time: {elapsed} seconds")
+    row_count = await update(loop=loop, sql="UPDATE bsc "
+                                            "SET nr_transactions = %s, balance = %s, nr_token_transfers = %s "
+                                            "WHERE address = %s",
+                             pool=pool, values=values)
+
+    end = time.time()
+    elapsed = end - start
+    logging.info("Finished crawling bscscan")
+    logging.info(f"{row_count} record(s) affected.")
+    logging.info(f"Elapsed time: {elapsed} seconds")
+
+    pool.close()
+    await pool.wait_closed()
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    cur_loop = asyncio.get_event_loop()
+    cur_loop.run_until_complete(main(cur_loop))
+    # asyncio.run(main())
