966262a0a841a2f20b0ba7f4f46be535ae2c6822
mchara01 <mc921@ic.ac.uk>
2022-08-02 14:03:51 +0100

    execution text files updated

--- scripts/database/queries.py
+++ scripts/database/queries.py
@@ -11,9 +11,12 @@ QUERIES = {
     "find_unique_bytecodes_eth": "SELECT COUNT(DISTINCT(bytecode)) FROM eth;",
     "find_unique_bytecodes_bsc": "SELECT COUNT(DISTINCT(bytecode)) FROM bsc;",
 
+    "find_unique_bytecodes_filtered_eth": "SELECT COUNT(DISTINCT(bytecode)) FROM eth WHERE balance > 0 or nr_token_transfers > 0 or nr_transactions > 0;",
+    "find_unique_bytecodes_filtered_bsc": "SELECT COUNT(DISTINCT(bytecode)) FROM bsc WHERE balance > 0 or nr_token_transfers > 0 or nr_transactions > 0;",
+
     "contracts_with_balance_eth": "SELECT address,balance FROM eth WHERE balance > 0;",
     "contracts_with_balance_bsc": "SELECT address,balance FROM bsc WHERE balance > 0;",
 
     "top_balance_eth": "SELECT address, balance FROM eth WHERE balance > 0 ORDER BY balance DESC LIMIT 10",
-    "top_balance_bsc": "SELECT address, balance FROM bsc WHERE balance > 0 ORDER BY balance DESC LIMIT 10",
+    "top_balance_bsc": "SELECT address, balance FROM bsc WHERE balance > 0 ORDER BY balance DESC LIMIT 10"
 }
